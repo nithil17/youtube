29e38dd4d0e6ef6446b6851d7357c1808b269aa3
nithil17 <nithilnayan@yahoo.com>
2026-06-09 16:28:23 +0530

    added files for pages folder to maintain ruf strucutre

---
+++ client/src/components/VideoGrid/VideoGrid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VideoGrid = () => {
+  return (
+    <div>VideoGrid</div>
+  )
+}
+
+export default VideoGrid
---
+++ client/src/utils/videos.js
@@ -0,0 +1,18 @@
+export const videos = [
+  {
+    id: 1,
+    title: "Learn React in 30 Minutes",
+    channel: "Code Academy",
+    views: "15K",
+    thumbnail:
+      "https://picsum.photos/400/250"
+  },
+  {
+    id: 2,
+    title: "JavaScript Interview",
+    channel: "Dev Talks",
+    views: "42K",
+    thumbnail:
+      "https://picsum.photos/400/251"
+  }
+];
