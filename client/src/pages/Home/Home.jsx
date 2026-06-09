1e8d580210add2a89ce8a6f4f171fbab24141d1d
nithil17 <nithilnayan@yahoo.com>
2026-06-09 22:07:36 +0530

    updated home and home css

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,22 +3,29 @@ import Header from '../../components/Header/Header'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import FilterBar from '../../components/FilterBar/FilterBar'
 import VideoGrid from '../../components/VideoGrid/VideoGrid'
+import "./Home.css"
 
 const Home = () => {
 
     const [searchText, setSearchText] = useState("");
-
+    
   return (
     <>
     <Header
         searchText={searchText}
         setSearchText = {setSearchText}
     />
-    <p>Current Search: {searchText}</p>
-    <Sidebar/>
-    <FilterBar/>
-    <VideoGrid 
-        searchText={searchText}/>
+        <main className='home-container'>
+    
+            <Sidebar/>
+            <section className='content'>
+                <FilterBar/>
+                <VideoGrid 
+                    searchText={searchText}/>
+            </section>
+            
+        </main>
+  
     </>
   )
 }
