87f9e6a3231d16ed8fdd70b205632386ab7c0c4e
nithil17 <nithilnayan@yahoo.com>
2026-06-10 21:12:58 +0530

    added links to videocards

--- client/src/components/VideoCard/VideoCard.jsx
+++ client/src/components/VideoCard/VideoCard.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import "./VideoCard.css"
+import { Link } from 'react-router-dom'
 
-const VideoCard = ({video}) => {
+const VideoCard = ({ video }) => {
   return (
-    <div className="video-card">
-        <img 
-          src={video.thumbnail} 
+    <Link 
+      to={`/watch/${video.id}`}
+      className='video-link'
+    >
+      <div className="video-card">
+        <img
+          src={video.thumbnail}
           alt={video.title}
           className='video-thumbnail'
         />
         <div>
-           <h3>{video.title}</h3>
+          <h3>{video.title}</h3>
 
-            <p>{video.channel}</p>
+          <p>{video.channel}</p>
 
-            <p>{video.views} views</p>
+          <p>{video.views} views</p>
 
         </div>
-         
-    </div>
+
+      </div>
+    </Link>
+
   )
 }
 
