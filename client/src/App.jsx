74175ed955a49f388f6ef8effea042a643e190aa
nithil17 <nithilnayan@yahoo.com>
2026-06-10 20:16:38 +0530

    updated app jsx with browser router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,22 @@
 
 import './App.css'
 import Home from './pages/Home/Home';
+import { BrowserRouter, Route } from "react-router-dom";
+
+<BrowserRouter>
+  <Routes>
+    <Route
+      path = "/"
+      element = {Home}
+    />
+
+    <Route
+      path='/watch/:id'
+      element={<VideoPlayer />}
+    />
+    
+  </Routes>
+</BrowserRouter>
 
 function App() {
 
