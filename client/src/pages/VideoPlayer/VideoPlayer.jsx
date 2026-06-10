bb68fab3f748e59399f061ebfd12e15fb206d0a8
nithil17 <nithilnayan@yahoo.com>
2026-06-10 21:26:28 +0530

    added new feature to handle and display video details in video player page

--- client/src/pages/VideoPlayer/VideoPlayer.jsx
+++ client/src/pages/VideoPlayer/VideoPlayer.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
+import { videos } from '../../utils/videos';
 
 const VideoPlayer = () => {
+
+  const {id} = useParams()
+
+  const  selectedVideo = videos.find((video)=>{
+    return video.id === Number(id);
+  })
+  
+  
   return (
-    <div>VideoPlayer</div>
+    <div className='video-player'>
+      
+      <img 
+      src={selectedVideo.thumbnail} 
+      alt={selectedVideo.title} />
+
+      <h2>
+        {selectedVideo.title}
+      </h2>
+
+      <p>
+        {selectedVideo.channel}
+
+      </p>
+
+      <p>{selectedVideo.views}Views</p>
+      
+      
+      </div>
   )
 }
 
