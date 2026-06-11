1fdd61cb183d30186d2c001d6fce04ac5a7890a1
nithil17 <nithilnayan@yahoo.com>
2026-06-11 13:40:49 +0530

    updated vidoePlayer jsx with new ui and updated css

--- client/src/pages/VideoPlayer/VideoPlayer.jsx
+++ client/src/pages/VideoPlayer/VideoPlayer.jsx
@@ -1,37 +1,59 @@
 import React from 'react'
 import { useParams } from "react-router-dom";
 import { videos } from '../../utils/videos';
-
+import "./VideoPlayer.css"
+import Header from '../../components/Header/Header';
+import { useState } from 'react';
 const VideoPlayer = () => {
-
-  const {id} = useParams()
-
-  const  selectedVideo = videos.find((video)=>{
+  const { id } = useParams()
+  const selectedVideo = videos.find((video) => {
     return video.id === Number(id);
   })
-  
-  
+  const relatedVideos = videos.filter((video)=>{
+    return video.id !== Number(id);
+  })
   return (
-    <div className='video-player'>
-      
-      <img 
-      src={selectedVideo.thumbnail} 
-      alt={selectedVideo.title} />
+    <div>
+      <Header />
 
-      <h2>
-        {selectedVideo.title}
-      </h2>
+      <div className="video-player-page">
+        <div className="main-video">
+          <img
+            className="player-thumbnail"
+            src={selectedVideo.thumbnail}
+            alt={selectedVideo.title}
+          />
 
-      <p>
-        {selectedVideo.channel}
+          <div className='video-info'>
+            <h2>{selectedVideo.title}</h2>
+            <div className='video-meta'>
+              <p>{selectedVideo.channel}</p>
+              <p>{selectedVideo.views} Views</p>
+            </div>
+          </div>
 
-      </p>
+        </div>
 
-      <p>{selectedVideo.views}Views</p>
-      
-      
+        <div className="related-videos">
+          <h3>Related Videos</h3>
+          {
+            relatedVideos.map((video)=>{
+              return (
+                <div key={video.id}>
+                  <img
+
+                  src={video.thumbnail}
+
+                  alt={video.} />
+                </div>
+
+              )
+            })
+          }
+        </div>
+
       </div>
+    </div>
   )
 }
-
 export default VideoPlayer
